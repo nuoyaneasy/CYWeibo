bc05482fe0745f0de22064b936cf33974fc5228e
Chao Yang <doublemeltin@gmail.com>
2015-06-07 10:55:18 +0800

    Added placeholder textView

---
+++ Weibo/Classes/Main/View/MultilineTextView.h
@@ -0,0 +1,13 @@
+//
+//  MultilineTextView.h
+//  Weibo
+//
+//  Created by Yang Chao on 6/7/15.
+//  Copyright (c) 2015 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MultilineTextView : UITextView
+
+@end
---
+++ Weibo/Classes/Main/View/MultilineTextView.m
@@ -0,0 +1,21 @@
+//
+//  MultilineTextView.m
+//  Weibo
+//
+//  Created by Yang Chao on 6/7/15.
+//  Copyright (c) 2015 Self. All rights reserved.
+//
+
+#import "MultilineTextView.h"
+
+@implementation MultilineTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
