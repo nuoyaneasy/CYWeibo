cca3aacf0ef3fbecedcd673745175fd771b9bf90
Chao Yang <doublemeltin@gmail.com>
2015-06-12 14:04:52 +0800

    const

---
+++ Weibo/const.m
@@ -0,0 +1,12 @@
+//
+//  const.m
+//  Weibo
+//
+//  Created by Yang Chao on 6/12/15.
+//  Copyright © 2015 Self. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+NSString * const Appkey = @"2489662495";
+
+
